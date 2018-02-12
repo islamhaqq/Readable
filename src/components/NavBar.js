12a7d0f5e728b7bd337bab2c92c659aa26b258b4
Syed Aman <sna274@nyu.edu>
2018-02-12 06:49:19 -0600

    create the navbar component and its static content

---
+++ src/components/NavBar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const NavBar = () => (
+	<tr className="navigation-bar-container">
+		<td bgcolor="#008000">
+			<table className="navigation-bar-content">
+				<tbody>
+					<tr>
+						<td className="navigation-bar-main-links">
+							<a href="#">Readable</a>
+							<a href="#">Ask</a>
+							<a href="#">Non-Fiction</a>
+							<a href="#">Fiction</a>
+						</td>
+						<td className="navigation-bar-profile-actions">
+							<a href="#">Syed Aman</a>
+							<a href="#">logout</a>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		</td>
+	</tr>
+);
+
+export default NavBar;
