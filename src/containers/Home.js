8a629eb123b378b0cbd67ff4c6beb195c93ca40c
Syed Aman <sna274@nyu.edu>
2018-02-12 06:56:16 -0600

    add static content for the home page

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -3,9 +3,17 @@ import React, { Component } from 'react';
 class Home extends Component {
 	render() {
 		return (
-			<div>
-				<h1>Home</h1>
-			</div>
+			<tr className="home-container">
+				<td>
+					<table className="home-content">
+						<tbody>
+							<tr className="post-title-row" />
+							<tr className="post-details-row" />
+							<tr className="post-spacer-row" />
+						</tbody>
+					</table>
+				</td>
+			</tr>
 		);
 	}
 }
