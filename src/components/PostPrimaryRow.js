5367939cfba822ff75fc4e25c3037cd27ebbd577
Syed Aman <sna274@nyu.edu>
2018-02-16 05:34:49 -0600

    componentize the primary row

---
+++ src/components/PostPrimaryRow.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const PostPrimaryRow = () => (
+	<tr className="post-main-row">
+		<td className="post-rank">
+			<span>1.</span>
+		</td>
+
+		<td className="post-upvote-container">
+			<span>^</span>
+		</td>
+
+		<td className="post-title">Let's talk about usernames</td>
+	</tr>
+);
+
+export default PostPrimaryRow;
