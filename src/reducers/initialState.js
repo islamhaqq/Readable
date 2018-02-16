fa12646a94188d157c7017f6ca3e9ea0be055a9b
Syed Aman <sna274@nyu.edu>
2018-02-16 07:13:18 -0600

    update initial state

--- src/reducers/initialState.js
+++ src/reducers/initialState.js
@@ -22,27 +22,31 @@ const initialState = {
 				comments: ['comment2'],
 			},
 		},
+		allIds: ['post1', 'post2'],
 	},
-	comments: [
-		{
-			author: 'tabtilachowdhury',
-			timestamp: Date.now(),
-			id: 'comment1',
-			body: 'this is my comment body...',
-			points: 50,
-			isDeleted: false,
-			timestamp: Date.now(),
+	comments: {
+		byId: {
+			comment1: {
+				author: 'tabtilachowdhury',
+				timestamp: Date.now(),
+				id: 'comment1',
+				body: 'this is my comment body...',
+				points: 50,
+				isDeleted: false,
+				timestamp: Date.now(),
+			},
+			comment2: {
+				author: 'asdads',
+				timestamp: Date.now(),
+				id: 'comment2',
+				body: 'this is my another comment body...',
+				points: 61,
+				isDeleted: false,
+				timestamp: Date.now(),
+			},
 		},
-		{
-			author: 'asdads',
-			timestamp: Date.now(),
-			id: 'comment2',
-			body: 'this is my another comment body...',
-			points: 61,
-			isDeleted: false,
-			timestamp: Date.now(),
-		},
-	],
+		allIds: ['comment1', 'comment2'],
+	},
 };
 
 export default initialState;
