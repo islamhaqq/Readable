b2f7632b67abd462552bf8b4506cc7bd6652c592
Syed Aman <saman@elevate.com>
2018-02-16 12:21:26 -0600

    provide validation for TableSpacerRow

--- src/components/TableSpacerRow.js
+++ src/components/TableSpacerRow.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const TableSpacerRow = ({ height }) => (
 	<tr style={{ height: height }} className="post-spacer-row" />
 );
+
+TableSpacerRow.propTypes = {
+	height: PropTypes.string.isRequired,
+};
 
 export default TableSpacerRow;
