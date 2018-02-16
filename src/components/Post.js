6da45ff40246c86072af5fafd92f30f5ab696d55
Syed Aman <sna274@nyu.edu>
2018-02-16 05:34:35 -0600

    componentize the primary row

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,19 +1,12 @@
 import React from 'react';
 
+import PostPrimaryRow from './PostPrimaryRow';
 import PostSpacerRow from './PostSpacerRow';
 import PostDetailsRow from './PostDetailsRow';
 
 const Post = () => (
 	<tbody className="post-container">
-		<tr className="post-main-row">
-			<td className="post-rank">
-				<span>1.</span>
-			</td>
-			<td className="post-upvote-container">
-				<span>^</span>
-			</td>
-			<td className="post-title">Let's talk about usernames</td>
-		</tr>
+		<PostPrimaryRow />
 
 		<PostDetailsRow />
 
