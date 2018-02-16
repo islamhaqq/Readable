fe703e2026acb71020c6f45ea7675cd11795f90e
Syed Aman <saman@elevate.com>
2018-02-16 12:15:35 -0600

    add in upvoting functionality to the posts reducer

--- src/reducers/allReducers.js
+++ src/reducers/allReducers.js
@@ -5,7 +5,10 @@ export function posts(previousState = initialState, action) {
 	switch (action.type) {
 		case UPVOTE_POST_ACTION_TYPE:
 			return Object.assign({}, previousState, {
-				...previousState,
+				previousState.posts.byId[action.postId]: {
+					...previousState.posts.byId[action.postId],
+					points: ...previousState.posts.byId[action.postId].points + 1
+				}
 			});
 		default:
 			return previousState;
