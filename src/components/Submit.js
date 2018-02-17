b7238194da3568a3d9ec92ff00c61aa2a9cc7289
Syed Aman <sna274@nyu.edu>
2018-02-17 08:21:14 -0600

    add in a formfield and navbar for submit

--- src/components/Submit.js
+++ src/components/Submit.js
@@ -1,10 +1,35 @@
 import React from 'react';
 
 const Submit = () => (
-	<div>
-		<label>Title</label>
-		<input type="text" />
-	</div>
+	<center>
+		<table>
+			<tbody>
+				<tr>
+					<td bgcolor="#ff6600">
+						<table
+							border="0"
+							cellpadding="0"
+							cellspacing="0"
+							width="100%"
+							style={{ padding: '2px' }}
+						>
+							<tbody>
+								<tr>
+									<td style={{ 'line-height': '12pt', height: '10px' }}>
+										<span>
+											<b>Submit</b>
+										</span>
+									</td>
+								</tr>
+							</tbody>
+						</table>
+					</td>
+				</tr>
+			</tbody>
+			<label>Title</label>
+			<input type="text" />
+		</table>
+	</center>
 );
 
 export default Submit;
