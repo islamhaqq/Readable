253e4a4ac51dc6619fe241c011fdf40a474095b6
Syed Aman <sna274@nyu.edu>
2018-02-17 07:20:57 -0600

    use momentjs to display how much time has past since posted

--- src/components/PostDetailsRow.js
+++ src/components/PostDetailsRow.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 
 const PostDetailsRow = ({ post }) => (
 	<tr className="post-details-row">
@@ -8,9 +9,13 @@ const PostDetailsRow = ({ post }) => (
 		<td className="post-details-container">
 			<span className="post-points">{`${post.points} points`}</span>
 			<a href="#" className="post-author">
-				syedaman
+				{post.author}
 			</a>
-			<span className="post-timestamp">7 hours ago</span>
+			<span className="post-timestamp">
+				{moment(post.timestamp)
+					.startOf('second')
+					.fromNow()}
+			</span>
 			|
 			<a className="post-details-view-comments-link">218 comments</a>
 		</td>
