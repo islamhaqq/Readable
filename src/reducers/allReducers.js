18f841d453f019cdb51bcca34d685c20c711b76b
Syed Aman <sna274@nyu.edu>
2018-02-19 14:21:13 -0600

    indicate the app is loading when comments are being fetched

--- src/reducers/allReducers.js
+++ src/reducers/allReducers.js
@@ -236,15 +236,19 @@ export function comments(previousState = initialState.comments, action) {
 export function general(previousState = initialState.general, action) {
 	switch (action.type) {
 		case actionTypes.REQUEST_ALL_POSTS_ACTION_TYPE:
+		case actionTypes.REQUEST_COMMENTS_FOR_POST_ACTION_TYPE:
 			return {
 				...previousState,
 				isLoading: true,
 			};
+
 		case actionTypes.RECEIVE_ALL_POSTS_ACTION_TYPE:
+		case actionTypes.RECEIVE_COMMENTS_FOR_POST_ACTION_TYPE:
 			return {
 				...previousState,
 				isLoading: false,
 			};
+
 		default:
 			return previousState;
 	}
