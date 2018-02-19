47403f42c6316172d87108e797edbda9d8c3e36d
Syed Aman <sna274@nyu.edu>
2018-02-19 10:08:26 -0600

    fix typo where redux dev tools wasn't used properly

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,7 @@ import { createLogger } from 'redux-logger';
 import rootReducer from '../reducers';
 
 const composeEnhancersWithReduxDevTools =
-	window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE__ || compose;
+	window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const loggerMiddleware = createLogger();
 
