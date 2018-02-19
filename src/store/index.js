2e490949e6ecca8e348b6ae456636af0a280f20c
Syed Aman <sna274@nyu.edu>
2018-02-19 07:48:26 -0600

    apply the thunk and action logging middleware into the store

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,15 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
 
 import rootReducer from '../reducers';
 
+const loggerMiddleware = createLogger();
+
 const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-)
+	rootReducer,
+	applyMiddleware(thunkMiddleware, loggerMiddleware),
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
 
 export default store;
