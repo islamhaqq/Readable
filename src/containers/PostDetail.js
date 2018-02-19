a213148b2463bbe8389e7eadcd2708313402f34a
Syed Aman <sna274@nyu.edu>
2018-02-19 14:22:49 -0600

    display comments that are loaded from the api

--- src/containers/PostDetail.js
+++ src/containers/PostDetail.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import uniqid from 'uniqid';
 import moment from 'moment';
@@ -7,6 +8,7 @@ import postPropType from '../utils/proptypes/postPropType';
 import Post from '../components/Post';
 import UpvoteButton from '../components/UpvoteButton';
 import DownvoteButton from '../components/DownvoteButton';
+import * as actionCreators from '../actions';
 
 class PostDetail extends Component {
 	constructor(props) {
@@ -16,6 +18,15 @@ class PostDetail extends Component {
 			isEditDialogShowing: false,
 			updatedPostBody: '',
 		};
+	}
+
+	async componentDidMount() {
+		const response = await this.props.fetchComments(this.props.post.id);
+		const comments = response.payload.comments;
+
+		this.setState({
+			comments,
+		});
 	}
 
 	handleUpdatedPostBodyChange = event => {
@@ -35,7 +46,6 @@ class PostDetail extends Component {
 	render() {
 		const {
 			post,
-			comments,
 			onUpvotePost,
 			onDownvotePost,
 			onUpvoteComment,
@@ -105,88 +115,89 @@ class PostDetail extends Component {
 									<div className="comment-section">
 										<h2 className="comment-section-header">Comments</h2>
 										<ul className="comment-section-comment-list">
-											{comments.map(comment => {
-												const {
-													id,
-													author,
-													body,
-													voteScore,
-													timestamp,
-												} = comment;
+											{this.state.comments &&
+												this.state.comments.map(comment => {
+													const {
+														id,
+														author,
+														body,
+														voteScore,
+														timestamp,
+													} = comment;
 
-												return (
-													<li key={id} className="comment">
-														<h6 className="comment-author">
-															{`${voteScore} points by ${author} ${moment(
-																timestamp,
-															)
-																.startOf('second')
-																.fromNow()}`}
-															<span
-																onClick={async () => {
-																	try {
-																		await this.setState(previousState => ({
-																			isEditDialogShowing: !previousState.isEditDialogShowing,
-																			commentBeingEditted: id,
-																			updatedPostBody: body,
-																		}));
-																	} catch (error) {
-																		throw new Error(error);
-																	}
-																}}
-																style={{ paddingLeft: '10px' }}
-															>
-																✎
-															</span>
-															<span
-																onClick={() => {
-																	onDeleteCommentFromPost({
-																		id,
-																		postId: post.id,
-																	});
-																}}
-																style={{ paddingLeft: '10px' }}
-															>
-																❌
-															</span>
-														</h6>
-
-														{this.state.isEditDialogShowing &&
-														this.state.commentBeingEditted === id ? (
-															<div>
-																<textarea
-																	onChange={this.handleUpdatedPostBodyChange}
-																	value={this.state.updatedPostBody}
-																	row="4"
-																	col="50"
-																/>
-																<button
+													return (
+														<li key={id} className="comment">
+															<h6 className="comment-author">
+																{`${voteScore} points by ${author} ${moment(
+																	timestamp,
+																)
+																	.startOf('second')
+																	.fromNow()}`}
+																<span
+																	onClick={async () => {
+																		try {
+																			await this.setState(previousState => ({
+																				isEditDialogShowing: !previousState.isEditDialogShowing,
+																				commentBeingEditted: id,
+																				updatedPostBody: body,
+																			}));
+																		} catch (error) {
+																			throw new Error(error);
+																		}
+																	}}
+																	style={{ paddingLeft: '10px' }}
+																>
+																	✎
+																</span>
+																<span
 																	onClick={() => {
-																		this.updateComment({
+																		onDeleteCommentFromPost({
 																			id,
-																			body: this.state.updatedPostBody,
+																			postId: post.id,
 																		});
 																	}}
-																	type="submit"
+																	style={{ paddingLeft: '10px' }}
 																>
-																	Update Comment
-																</button>
-															</div>
-														) : (
-															<p className="comment-author">{body}</p>
-														)}
+																	❌
+																</span>
+															</h6>
 
-														<UpvoteButton
-															thingToUpvote={comment}
-															onUpvote={onUpvoteComment}
-														/>
-														<DownvoteButton
-															thingToDownvote={comment}
-															onDownvote={onDownvoteComment}
-														/>
-													</li>
-												);
-											})}
+															{this.state.isEditDialogShowing &&
+															this.state.commentBeingEditted === id ? (
+																<div>
+																	<textarea
+																		onChange={this.handleUpdatedPostBodyChange}
+																		value={this.state.updatedPostBody}
+																		row="4"
+																		col="50"
+																	/>
+																	<button
+																		onClick={() => {
+																			this.updateComment({
+																				id,
+																				body: this.state.updatedPostBody,
+																			});
+																		}}
+																		type="submit"
+																	>
+																		Update Comment
+																	</button>
+																</div>
+															) : (
+																<p className="comment-author">{body}</p>
+															)}
+
+															<UpvoteButton
+																thingToUpvote={comment}
+																onUpvote={onUpvoteComment}
+															/>
+															<DownvoteButton
+																thingToDownvote={comment}
+																onDownvote={onDownvoteComment}
+															/>
+														</li>
+													);
+												})}
 										</ul>
 									</div>
 								</td>
@@ -199,16 +210,21 @@ class PostDetail extends Component {
 	}
 }
 
+const mapStateToProps = currentState => ({});
+
+const mapDispatchToProps = dispatch => ({
+	fetchComments: postId => dispatch(actionCreators.fetchCommentsForPost(postId)),
+});
+
 Post.propTypes = {
 	post: postPropType,
 	onUpvotePost: PropTypes.func.isRequired,
 	onDownvotePost: PropTypes.func.isRequired,
 	onUpvoteComment: PropTypes.func.isRequired,
 	onDownvoteComment: PropTypes.func.isRequired,
-	comments: PropTypes.array.isRequired,
 	onAddCommentToPost: PropTypes.func.isRequired,
 	onDeleteCommentFromPost: PropTypes.func.isRequired,
 	onEditCommentOnPost: PropTypes.func.isRequired,
 };
 
-export default PostDetail;
+export default connect(mapStateToProps, mapDispatchToProps)(PostDetail);
