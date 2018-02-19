cd94fc48749760bb6687dc08835a935717092009
Syed Aman <sna274@nyu.edu>
2018-02-19 16:12:16 -0600

    fix issue where accept and content type headers weren't present

--- src/utils/config.js
+++ src/utils/config.js
@@ -3,5 +3,7 @@ export const apiUrl = 'http://localhost:3001';
 export const authorizationHeaders = {
 	headers: {
 		Authorization: 'whatever-you-want',
+		'Content-Type': 'application/json',
+		Accept: 'application/json',
 	},
 };
