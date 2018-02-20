38d8196bcc2426db9716abec7d90affff030cb18
Syed Aman <sna274@nyu.edu>
2018-02-20 07:55:12 -0600

    sort posts by date and score

--- src/components/PostsBoard.js
+++ src/components/PostsBoard.js
@@ -1,26 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import Post from '../components/Post';
-const PostsBoard = ({ posts, onUpvotePost, onDownvotePost }) => (
-	<tr className="home-container">
-		<td>
-			<table className="home-content">
-				{posts.map((post, index) => (
-					<Post
-						post={post}
-						onUpvote={onUpvotePost}
-						onDownvote={onDownvotePost}
-						rank={index + 1}
-						key={index}
-						isRankDisplayed={true}
-						isVoteButtonDisplayed={true}
-					/>
-				))}
-			</table>
-		</td>
-	</tr>
-);
+class PostsBoard extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			sortedBy: '',
+		};
+	}
+
+	render() {
+		const { posts, onUpvotePost, onDownvotePost } = this.props;
+
+		return (
+			<tr className="home-container">
+				<td>
+					<button
+						onClick={() => {
+							posts.sort((first, second) => second.voteScore - first.voteScore);
+							this.setState({ sortedBy: 'voteScore' });
+						}}
+					>
+						Hot
+					</button>
+					<button
+						onClick={() => {
+							posts.sort((first, second) => second.timestamp - first.timestamp);
+							this.setState({ sortedBy: 'date' });
+						}}
+					>
+						Newest
+					</button>
+
+					<table className="home-content">
+						{posts.map((post, index) => (
+							<Post
+								post={post}
+								onUpvote={onUpvotePost}
+								onDownvote={onDownvotePost}
+								rank={index + 1}
+								key={index}
+								isRankDisplayed={true}
+								isVoteButtonDisplayed={true}
+							/>
+						))}
+					</table>
+				</td>
+			</tr>
+		);
+	}
+}
 
 PostsBoard.propTypes = {
 	posts: PropTypes.array.isRequired,
