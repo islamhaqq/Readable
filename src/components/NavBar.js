713483b51bcfac4bc1d84540e970ced09ced6e09
Syed Aman <sna274@nyu.edu>
2018-02-19 20:01:02 -0600

    update navbar with category

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const NavBar = ({ categories }) => (
+const NavBar = ({ categories, currentCategory }) => (
 	<tr className="navigation-bar-container">
 		<td bgcolor="#008000">
 			<table className="navigation-bar-content">
@@ -9,12 +9,19 @@ const NavBar = ({ categories }) => (
 					<tr>
 						<td className="navigation-bar-main-links">
 							<Link to="/">Readable</Link>
-							{categories.map(category => (
-								<Link to={`/${category.path}`} style={{ paddingLeft: '10px' }}>
+							{categories.map((category, index) => (
+								<Link
+									to={`/${category.path}`}
+									key={index}
+									style={{ paddingLeft: '10px' }}
+								>
 									{category.name}
 								</Link>
 							))}
-							<Link to="/submit" style={{ paddingLeft: '10px' }}>
+							<Link
+								to={`${currentCategory}/submit`}
+								style={{ paddingLeft: '10px' }}
+							>
 								Submit
 							</Link>
 						</td>
